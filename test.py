625d4dd12a7fd17ee334dcfe975dfecf76dd4ec4
Diego <salas.diego@pucp.pe>
2017-12-11 20:50:46 -0500

    post request

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 #Author Diego Salas
 #Date 11/12/2017 
 #path /test
+from cgi import parse_qs, escape
+import json
 
 def service(request_body):
-	return "{\"test\":\"this is a test json\"}"
+	dictionary = parse_qs(request_body)
+	return json.dumps(dictionary)
+	#param = dictionary['param']
+	#return "{\"test\":\"this is a test json: " + param +  "\"}"
