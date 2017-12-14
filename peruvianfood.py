4ccc35e86721bf53baad92f01b7c4a3435ef477b
Rodrigo Maldonado C <32023142+TheCidRMC@users.noreply.github.com>
2017-12-14 16:56:39 -0500

    funcion find de peruvianfood

--- peruvianfood.py
+++ peruvianfood.py
@@ -100,6 +100,35 @@ def scrap_wikipedia():
     print(s4_url)
     return s4_strips
 
+def delete_stop_words(strips):
+    with open('stopwordsES.txt','r') as f:
+        stopwords = f.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    stopwords = [x.strip() for x in stopwords]
+    aux_strips = []
+    flg_stwd = 0
+    word_erase = []
+    for food in strips:
+        for word in food.split():
+            if word in stopwords:
+                food.replace(" ","")
+                flg_stwd = 1
+                word_erase.append(word)
+        if flg_stwd != 1:
+            aux_strips.append(food)
+        else:
+            n_food = food
+            for w in word_erase:
+                #n_food = n_food.replace(w,"")
+                n_food = re.sub(r"\b%s\b" % w, "", n_food)
+            aux_strips.append(n_food)
+
+    n_strips = []
+    for strip in aux_strips:
+        n_strips.append(re.sub(' +',' ',strip))
+    print(n_strips)
+    return n_strips
+
 def scrap_peruvian_food():
     #scrap 4 sources of peruvian food and drinks
     s1_strips = scrap_yanuq()
@@ -110,43 +139,61 @@ def scrap_peruvian_food():
     all_strips = s3_strips + s4_strips + s2_strips + s1_strips
     all_strips.sort()
     all_strips = list(set(all_strips))
+    # delete stop words
+    all_strips = delete_stop_words(all_strips)
     #save peruvian dishes and drinks corpus
     perfydcorpus = open('perfydcorpus.txt', 'wb')
     pickle.dump(all_strips, perfydcorpus)
     perfydcorpus.close()
     print("Número de platos y bebidas extraídos: " + str(len(all_strips)))
 
-def find_peruvian_dish_drink(food):
+def find_peruvian_dish_drink(tips):
     perfydcorpus = open('perfydcorpus.txt', 'rb')
     corpus = pickle.load(perfydcorpus)
-    match = [True for food in corpus]
+    corpus.sort()
+    output = {}
+    for tip in tips:
+        output[tip] = []
+        for bigram in tips[tip]:
+            flg = False
+            s_word = ''
+            for word in bigram:
+                s_word = s_word + " " + word
+            n_word = s_word.lower()
+            if n_word.strip() in corpus:
+                flg = True
+            output[tip].append(flg)
     perfydcorpus.close()
-
-scrap_peruvian_food()
-food = ['arroz con pollo','']
-find_peruvian_dish_drink("arroz con pollo")
-
-
-
-
+    return output
 
 """
-new_strips = []
-for i in all_strips:
-    #encontrar en la lista
-    #si no lo encontraste lo agregas, si si lo encontraste falso
-    for j in new_strips:
-    #Falta
-    new_strips.append(i)
-new_strips = [s.replace(u'\xa0', u'') for s in new_strips] #Eliminar caracter latin-1 \xa0 de items
-new_strips = [s.replace(u'\x93A', u'') for s in new_strips]
-new_strips = [s.replace(u'\x93', u'') for s in new_strips]
-new_strips = [s.replace(u'\x94', u'') for s in new_strips]
+Prueba
+tips  = {
+        "v1": [
+            [
+                "Un",
+                "buen"
+            ],
+            [
+                "pollo",
+                "brasa"
+            ],
 
-new_strips = [s.lower()for s in new_strips]
-new_strips = [s.strip()for s in new_strips]
-new_strips.sort()
+        ],
+        "v2": [
+            [
+                "aji",
+                "gallina"
+            ],
+            [
+                "grande",
+                "todo"
+            ],
+
+        ]
+        }
+print(find_peruvian_dish_drink(tips))
 """
 
 
-#print(new_strips[24] == new_strips[25])
+
