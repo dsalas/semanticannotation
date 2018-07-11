ce4c560b70ce3ebc0211892e124df9cada1602a8
DIA <salas.diego@pucp.pe>
2018-07-10 19:43:45 -0500

    Nltk download change dir

--- nltk_download.py
+++ nltk_download.py
@@ -1,3 +1,4 @@
 import nltk
-
-nltk.download('stopwords', download_dir='/var/www/nltk_data')
+import os
+path = os.path.join(os.path.dirname(__file__),"/nltk_data")
+nltk.download('stopwords', download_dir=path)
